7929bb44a8a3db65b4c1e5d157a1a9fa82dfd197
eorbezom <eorbezo99@gmail.com>
2024-07-27 18:41:35 -0500

    solo mensaje del commmit

---
+++ src/main/java/com/mycompany/techsavion/view/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.techsavion.view;
+
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("mi primer commit para el proyecto!");
+    }
+}
