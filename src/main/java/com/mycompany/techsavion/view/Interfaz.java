914dd664d34c3ea50733fb866bb9beeba5ae7b1e
eorbezom <eorbezo99@gmail.com>
2024-07-28 10:54:16 -0500

    pequeños modificaciones

--- src/main/java/com/mycompany/techsavion/view/Interfaz.java
+++ src/main/java/com/mycompany/techsavion/view/Interfaz.java
@@ -9,20 +9,20 @@ public class Interfaz {
         // Usar SwingUtilities para asegurar que la creación de la GUI se realice en el hilo de despacho de eventos
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                // Crear el marco principal
+                //el marco principal
                 JFrame frame = new JFrame("TechSavion");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                // Crear una etiqueta con el mensaje "Hola Mundo" 555
+                // Crear una etiqueta conel mensaje
                 JLabel label = new JLabel("Interfaz TechSavion:", JLabel.CENTER);
                 
-                // Agregar la etiqueta al marco
+                // la etiqueta al marco
                 frame.add(label);
 
-                // Establecer el tamaño del marco
+                //  el tamaño del marco
                 frame.setSize(800, 950);
                 
-                // Hacer el marco visible
+                // marco visible
                 frame.setVisible(true);
             }
         });
